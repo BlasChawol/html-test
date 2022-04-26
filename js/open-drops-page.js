0ef019bdf7bcbcf44a6edda9d170315eae8c7996
BlasChawol <blaschawol1@gmail.com>
2022-04-26 09:22:26 +0800

    1

--- js/open-drops-page.js
+++ js/open-drops-page.js
@@ -1,3 +1,12 @@
 function open_ferrite_page(){
     window.location.href="../html/ferrite.html"
 }
+function open_rubedo_page(){
+    window.location.href="../html/rubedo.html"
+}
+function open_neurodes_page(){
+    window.location.href="../html/neurodes.html"
+}
+function open_detoniteampule_page(){
+    window.location.href="../html/detoniteampule.html"
+}
