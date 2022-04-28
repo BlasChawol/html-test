3884aa12fc16d237565ea6d15899af6f5ef316f5
Blaschawol <blaschawol1@gmail.com>
2022-04-28 17:28:52 +0800

    Ver1.0.1-1

---
+++ js/back-index-button.js
@@ -0,0 +1,11 @@
+/*
+function back_1_step(){
+    windows.history.back(-1);
+}
+*/
+function back_to_index_page(){
+    window.location.href="../index.html";
+}
+function back_to_drop_page(){
+    window.location.href="../html/drops-main.html";
+}
